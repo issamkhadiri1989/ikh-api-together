ed40d2a8e440bd6ec0e2aa8311ca8fdb297f0f20
Issam KHADIRI <khadiri.issam@gmail.com>
2024-09-27 20:02:22 +0100

    stabilize the dependencies versions

---
+++ src/IkhApiTogetherBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ikh\ApiTogetherBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class IkhApiTogetherBundle extends AbstractBundle
+{
+
+}
